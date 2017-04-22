dd70418fdc6893931e4bbd834b15c98cb663a887
pluginernew <nut95@bk.ru>
2017-04-22 18:09:57 +0300

    Create Main.php

---
+++ src/tgrbh/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace tgrbh;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    if($sender->isOp()){
+      switch($cmd->getName()){
+        case 'cmd':
+          $sender->sendMessage(Color::WHITE."Hi");
+      } else {
+        $sender->sendMessage(Color::RED."You Not OP !!");
+      }
+    }
+  }
+}
